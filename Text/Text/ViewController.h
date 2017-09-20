6bfc8f9c0cf80387ea516b4f6731f3de953686af
dev <2303400083@qq.com>
2017-09-20 18:29:21 +0800

    友盟社会化主件

---
+++ Text/Text/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Text
+//
+//  Created by dev on 2017/9/20.
+//  Copyright © 2017年 dev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Text/Text/ViewController.m
@@ -0,0 +1,31 @@
+//
+//  ViewController.m
+//  Text
+//
+//  Created by dev on 2017/9/20.
+//  Copyright © 2017年 dev. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    self.view.backgroundColor = [UIColor orangeColor];
+    
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
